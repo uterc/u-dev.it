f41204d25810e52dafbadbf1568e4ed0fb0e62de
Cretu Laurentiu Adrian <cretu.laurentiu@gmail.com>
2021-05-10 19:46:25 +0200

    Update client.js

--- client.js
+++ client.js
@@ -25,7 +25,7 @@ async function send() {
 
   // Send Push Notification
   console.log("Sending Push...");
-  await fetch("http://93.40.10.227:5050/subscribe", {
+  await fetch("https://notify-lory.herokuapp.com/subscribe", {
     method: "POST",
     body: JSON.stringify(subscription),
     headers: {
